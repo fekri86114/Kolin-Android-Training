c4223b9dbdff5f8f419361174d61d95cc326421d
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-13 11:48:44 +0430

    init commit

--- app/src/main/java/com/app/kotlinandroidtraining/MainActivity.kt
+++ app/src/main/java/com/app/kotlinandroidtraining/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.app.kotlinandroidtraining
 
+import android.graphics.BlurMaskFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.RoundedCorner
 import android.widget.Toast
 import com.app.kotlinandroidtraining.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.lang.Exception
@@ -20,19 +24,28 @@ class MainActivity : AppCompatActivity() {
 
             val url = "https://dev.java/assets/images/java-logo-vert-blk.png"
 
-            Picasso.get()
+            Glide.with(this)
                 .load(url)
-                .error(R.drawable.broken_img)
-                .into(binding.imgMain, object: Callback{
-                    override fun onSuccess() {
-                        Toast.makeText(this@MainActivity, "HEY!!", Toast.LENGTH_SHORT).show()
-                    }
+//                .diskCacheStrategy( DiskCacheStrategy.AUTOMATIC )
+//                .placeholder(R.drawable.ic_launcher_background)
+//                .error(R.drawable.broken_img)
+//                .override(200, 200) // you can change image size
+//                .skipMemoryCache(true) // if it's true it doesn't cache on phone disk
+                .into(binding.imgMain)
 
-                    override fun onError(e: Exception?) {
-                        Toast.makeText(this@MainActivity, "DENIED!!", Toast.LENGTH_SHORT).show()
-                    }
-
-                })
+//            Picasso.get()
+//                .load(url)
+//                .error(R.drawable.broken_img)
+//                .into(binding.imgMain, object: Callback{
+//                    override fun onSuccess() {
+//                        Toast.makeText(this@MainActivity, "HEY!!", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                    override fun onError(e: Exception?) {
+//                        Toast.makeText(this@MainActivity, "DENIED!!", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                })
         }
     }
 }
